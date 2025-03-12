a4ff784f021de97c83f92ae91b76ad75cc4936e7
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-03-12 17:45:18 +0100

    Add project files.

---
+++ lab1/Problem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Problem
+    {
+        private int quantity;
+        private List<Przedmiot> items;
+
+        public Problem(int quantity, int seed)
+        {
+            this.quantity = quantity;
+            this.items = new List<Przedmiot>();
+            Random rand = new Random(seed);
+            for (int i = 0; i < quantity; i++)
+            {
+                Przedmiot item = new Przedmiot(i,rand.Next(1,10),rand.Next(1,10));
+                this.items.Add(item);
+            }
+        }
+
+        public Result Solve (int capacity)
+        {
+            Items.Sort((x, y) => y.Afordability.CompareTo(x.Afordability));
+            Result result = new Result();
+            for (int i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Weight + result.SumWeight + Items[i].Value+result.SumValue > capacity)
+                {
+                    break;
+                }
+                result.Numbers.Add(items[i].Id);
+                result.SumValue += Items[i].Value;
+                result.SumWeight += Items[i].Weight;
+                Items[i].IsTaken = true; 
+            }
+            return result;
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { this.quantity = value; }
+        }
+       
+
+        public List<Przedmiot> Items
+        {
+            get { return items; }
+            set { this.items = value; }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (Przedmiot item in this.items)
+            {
+                str += item.ToString() + "\n";
+            }
+            return str;
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of items:");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the seed:");
+            int seed = Convert.ToInt32(Console.ReadLine());
+
+            Problem problem = new Problem(quantity, seed);
+
+            Console.WriteLine(problem.ToString());
+
+            Console.WriteLine("Enter the capacity:");
+            int capacity = Convert.ToInt32(Console.ReadLine());
+
+            Result result = problem.Solve(capacity);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
